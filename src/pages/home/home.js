33ba15f08580e35a6944a31d770c9f1382c146e3
Bünyamin Mete <108274379+BunyaminMete@users.noreply.github.com>
2022-09-12 14:33:11 +0300

    first commit

---
+++ src/pages/home/home.js
@@ -0,0 +1,15 @@
+import './home.css';
+
+const SayHello = () => {
+  return (
+    <>
+      <div id="Home">
+        <p className="welcometext">
+          Hi I'm Bunyamin <br /> Welcome to My Page
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default SayHello;
