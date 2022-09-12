eaacf265143e5e78de66dd394c1939ec95a99617
Bünyamin Mete <108274379+BunyaminMete@users.noreply.github.com>
2022-09-12 15:14:23 +0300

    deploy update2

--- src/index.js
+++ src/index.js
@@ -4,12 +4,13 @@ import HomePage from './pages/header/home';
 import './style/main.css';
 import SayHello from './pages/home/home';
 import About from './pages/about/about';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <>
+  <BrowserRouter basename='/index.html'>
     <HomePage />
     <SayHello />
     <About />
-  </>,
+  </BrowserRouter>,
 );
